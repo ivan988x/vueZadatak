2782d8fcbec166f57c00c3273b53a21747e47714
Antonio <antonio.sertic@vsmti.hr>
2020-08-17 13:51:13 +0200

    nest malo

---
+++ src/components/SredinaStranice.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <div>
+      <h1>My Profil</h1>
+    </div>
+    <div class="products"></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.container {
+  width: 70%;
+  margin: 0 auto;
+  padding-bottom: 40px;
+}
+.container h1 {
+  font-weight: bold;
+  font-size: 45px;
+  line-height: 25px;
+  margin: 60px 0;
+}
+</style>
